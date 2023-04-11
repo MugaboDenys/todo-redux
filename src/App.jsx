d44f19bbd0ad2a5d45c5b481864cf710b06f8be7
MugaboDenys <mugabodan2020@gmail.com>
2023-04-11 16:41:36 +0200

    fixed inputs and removed warnings

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,6 @@ const App =()=> {
   const dispatch = useDispatch()
   const todos = useSelector((state)=> state.todo)
 
-  console.log(todos);
-  const handleInput =(event) => {
-    setInputValue(prevInput=> (prevInput, event.target.value) )
-  }
-  
 
   const handleEnter = (event) => {
     if(event.key === "Enter"){
@@ -31,18 +26,13 @@ const App =()=> {
     
     dispatch(addToDo({id : nanoid(), inputValue, complete : false}))
     setInputValue("")
-
-    // setInputValue("")
-    // return(
-    //   todo.unshift(<Todo id={nanoid()} handleDel={handleDelete} complete={false} check={toggleCompleted}>{inputValue}</Todo>)
-    // )
   }
   
   return (
     <div className="flex justify-center flex-col items-center mt-32">
       <h1 className="text-[10rem] font-bold text-gray-200">todos</h1>
       <div className="w-[32rem] h-10 rounded-full shadow-lg shadow-gray-300 items-center px-5 flex justify-between">
-        <input type="text" onChange={handleInput} onKeyDown={handleEnter} placeholder="Add todo..." value={inputValue} className="w-96 h-full outline-none placeholder:text-black" />
+        <input type="text" onChange={(event)=>{setInputValue(event.target.value)}} onKeyDown={handleEnter} placeholder="Add todo..." value={inputValue} className="w-96 h-full outline-none placeholder:text-black" />
         <AiFillPlusCircle className="text-green-700 text-2xl cursor-pointer" onClick={()=>addTodos()} />
       </div>
       <div className="">
